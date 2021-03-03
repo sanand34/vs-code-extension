36f4bbcf0d4480fd95250e77f239f1fe9fdaf565
sanand34 <sanchitanand34@gmail.com>
2021-03-04 03:28:32 +0530

    Initial Commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,28 @@
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "get-request" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "get-request" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('get-request.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Get request!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("get-request.helloWorld", () => {
+      HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("get-request.askQuestion", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that");
+      } else {
+        vscode.window.showInformationMessage("Go on!!!!!!");
+      }
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
